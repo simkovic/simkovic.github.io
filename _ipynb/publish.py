e4a52a22b0a3accb696eaabe5bded890b3c95189
matus <matus@quad.com>
2014-06-25 11:05:44 +0200

    almost new post

--- _ipynb/publish.py
+++ _ipynb/publish.py
@@ -7,7 +7,10 @@ except:
     sys.exit(-1)
 outfn=datetime.date.today().isoformat()+'-'+notebook.rstrip('.ipynb').replace(' ','-')
 outfn=os.getcwd().rstrip('.ipynb')+'posts'+os.path.sep+outfn
+remchar=['?','!']
+for ch in remchar: outfn=outfn.replace(ch,'')
 # convert notebook
+print 'ipython nbconvert --to html \"%s\" --output %s' % (notebook,outfn)
 os.system('ipython nbconvert --to html \"%s\" --output %s' % (notebook,outfn))
 html=open(outfn+'.html','r').read()
 html='''---\nlayout: post\ntitle:  %s\n---\n'''%notebook.rstrip('.ipynb') + html
