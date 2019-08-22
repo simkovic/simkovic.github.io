f7b1e3b3cdd493a58c9cb21958430fdeb5c36ef1
Matus <matus@archclevo>
2019-08-22 19:44:58 +0200

    upgrade to jupyterV4 and new post

--- _ipynb/reexportposts.py
+++ _ipynb/reexportposts.py
@@ -1,12 +1,12 @@
 import sys,os,datetime
 
-
+notebook='Guess what?! Another Analysis of the Schnall-Johnson Data.ipynb'
 outfn=datetime.date.today().isoformat()+'-'+notebook.rstrip('.ipynb').replace(' ','-')
 outfn=os.getcwd().rstrip('.ipynb')+'posts'+os.path.sep+outfn
 remchar=['?','!']
 for ch in remchar: outfn=outfn.replace(ch,'')
 # convert notebook
-print 'ipython nbconvert --to html \"%s\" --output %s' % (notebook,outfn)
+print('ipython nbconvert --to html \"%s\" --output %s' % (notebook,outfn))
 os.system('ipython nbconvert --to html \"%s\" --output %s' % (notebook,outfn))
 html=open(outfn+'.html','r').read()
 html='''---\nlayout: post\ntitle:  %s\n---\n'''%notebook.rstrip('.ipynb') + html
